a96335b4b69ba4dd72a7f40507cfee700ca87758
timdewilde <tim-de-wilde@live.nl>
2023-05-22 11:39:16 +0200

    roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -40,5 +41,11 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'role' => Role::class,
     ];
+
+    public function hasRole(Role $role): bool
+    {
+        return $this->role === $role;
+    }
 }
