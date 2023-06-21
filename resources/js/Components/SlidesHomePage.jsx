fadd64bfa9e0b07edd63fa3c4dcb546e2e76066d
KyraNhf <k.noordhof@st.hanze.nl>
2023-06-21 18:43:52 +0200

    WindSpeedTable.jsx verbeterd

---
+++ resources/js/Components/SlidesHomePage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import EuropeMap from "@/Components/MapEurope.jsx";
+
+export default class SlidesHomePage extends Component {
+    render() {
+        return (
+            <EuropeMap></EuropeMap>
+        );
+    }
+}
--- resources/js/Components/WindSpeedTable.jsx
+++ resources/js/Components/WindSpeedTable.jsx
@@ -1,50 +1,33 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-
-export function WindSpeedTable({ auth, topWindSpeeds }) {
+export function WindSpeedTable({topWindSpeeds}) {
     function createData(country, city, wind) {
         return {country, city, wind};
     }
+    console.log(topWindSpeeds);
+    const data = topWindSpeeds
+    const rows = data.map((station) => createData(station.nearest_location.administrative_region1, station.nearest_location.administrative_region2, data.wind_speed))
 
-    const rows = topWindSpeeds.map((station) =>
-        createData(station.nearest_location.administrative_region1,
-            station.nearest_location.administrative_region2,
-            data.wind_speed));
-
-    export default function WindSpeedTable() {
-        return (
-            <TableContainer component={Paper}>
-                <Table sx={{minWidth: 650}} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Country</TableCell>
-                            <TableCell align="right">City</TableCell>
-                            <TableCell align="right">Wind speed</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.country}
-                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.country}
-                                </TableCell>
-                                <TableCell align="right">{row.city}</TableCell>
-                                <TableCell align="right">{row.wind}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        );
-    }
+    var tableRows = rows.map((row) => (
+        <tr>
+            <td component="th" scope="row">{row.country}</td>
+            <td align="right">{row.city}</td>
+            <td align="right">{row.wind}</td>
+        </tr>))
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Country</th>
+                        <th align="right">City</th>
+                        <th align="right">Wind speed</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableRows}
+                </tbody>
+            </table>
+        </div>
+    )
 }
